fb2cc7d67de98b5fad7b5f7cab609e37536e8dff
ajrobbins04 <ajrobbins04@gmail.com>
2023-10-18 17:35:41 -0700

    cards.models populated w/math facts from 1-12

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+"""
+Stores all mathematical computations that
+will be used to create flashcards. The
+lhs and rhs values will span from 1 - 12.
+"""
+class Add(models.Model):
+    lhs = models.IntegerField()
+    rhs = models.IntegerField()
+    result = models.IntegerField()
+    operator = models.CharField(max_length=1)
+
+class Subtract(models.Model):
+    lhs = models.IntegerField()
+    rhs = models.IntegerField()
+    result = models.IntegerField()
+    operator = models.CharField(max_length=1)
+
+class Multiply(models.Model):
+    lhs = models.IntegerField()
+    rhs = models.IntegerField()
+    result = models.IntegerField()
+    operator = models.CharField(max_length=1)
+
+class Divide(models.Model):
+    lhs = models.IntegerField()
+    rhs = models.IntegerField()
+    result = models.IntegerField()
+    operator = models.CharField(max_length=1)
